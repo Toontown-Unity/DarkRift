ce4f7f88c3a2697033f5c8464539589a9d7820d5
Jace Mayer <davidallancrompton@outlook.com>
2024-08-18 14:27:00 +0100

    Add missing references in LightRift

--- DarkRift.Client/ClientObjectCacheTestHelper.cs
+++ DarkRift.Client/ClientObjectCacheTestHelper.cs
@@ -4,6 +4,8 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+using System.Threading;
+
 namespace DarkRift.Client
 {
 #if DEBUG
--- DarkRift/ObjectCacheTestHelper.cs
+++ DarkRift/ObjectCacheTestHelper.cs
@@ -4,6 +4,9 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+using System.Net.Sockets;
+using System.Threading;
+
 namespace DarkRift
 {
 #if DEBUG
